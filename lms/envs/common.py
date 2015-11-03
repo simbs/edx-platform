vikas1885/test1

Updated track selection page to use ecommerce payment flow
If the payment flow is enabled, verified purchases will be completed using the ecommerce checkout and payment pages.

ECOM-2473

lms/envs/common.py
@@ -2584,7 +2584,7 @@ ECOMMERCE_API_URL = None
 ECOMMERCE_API_SIGNING_KEY = None
 ECOMMERCE_API_TIMEOUT = 5
 ECOMMERCE_SERVICE_WORKER_USERNAME = 'ecommerce_worker'
-ECOMMERCE_COURSE_CHECKOUT_PATH = '/checkout/single-course/'
+ECOMMERCE_COURSE_CHECKOUT_PATH = '/checkout/single-item/'
 
 # Reverification checkpoint name pattern
 CHECKPOINT_PATTERN = r'(?P<checkpoint_name>[^/]+)'

simbs/edx-platform

lms/envs/common.py
@@ -2602,7 +2602,7 @@ ECOMMERCE_API_URL = None
 ECOMMERCE_API_SIGNING_KEY = None
 ECOMMERCE_API_TIMEOUT = 5
 ECOMMERCE_SERVICE_WORKER_USERNAME = 'ecommerce_worker'
-ECOMMERCE_COURSE_CHECKOUT_PATH = '/checkout/single-course/'
+ECOMMERCE_COURSE_CHECKOUT_PATH = '/basket/single-item/'
 
 # Reverification checkpoint name pattern
 CHECKPOINT_PATTERN = r'(?P<checkpoint_name>[^/]+)'