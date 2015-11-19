dc358d137164205e66b75620cea144a532da3d24
J. Cliff Dyer <cdyer@edx.org>
2015-11-19 18:52:11 +0000

    fixup: Convert view docs to API doc format.

--- lms/djangoapps/course_api/views.py
+++ lms/djangoapps/course_api/views.py
@@ -17,64 +17,81 @@ from .api import course_detail, list_courses
 @view_auth_classes()
 class CourseDetailView(APIView):
     """
-    View class to describe a course
-    """
+    **Use Cases**
 
-    def get(self, request, course_key_string):
-        """
-        Request information on a course specified by `course_key_string`.
+        Request information on a course
+
+    **Example Requests**
+
+        GET /api/courses/v1/courses/{course_key_string}/
+
+    **Response Values**
 
         Body consists of the following fields:
-            * `blocks_url`: can be used to fetch the blocks
-            * `course_image`:
-            * `description`:
-            * `end`: Date the course ends
-            * `enrollment_end`: Date enrollment ends
-            * `enrollment_start`: Date enrollment begins
-            * `id`: Course key
-            * `name`: Name of the course
-            * `number`: Catalog number of the course
-            * `org`: Name of the organization that owns the course
-            * `start`: Date the course begins
-            * `start_display`: Readably formatted start of the course
-            * `start_type`: Hint describing how `start_display` is set. One of:
-                  - `"string"`: manually set
-                  - `"timestamp"`: generated form `start` timestamp
-                  - `"empty"`: the start date should not be shown
 
-        Arguments:
-            request: (HttpRequest)
-            course_key_string: (string) Key for the desired course
+        * blocks_url: used to fetch the course blocks
+        * media: An object that contains named media items.  Included here:
+            * course_image: An image to show for the course.  Represented
+              as an object with the following fields:
+                * uri: The location of the image
+                * name:
+                * description:
+                * type:
+        * end: Date the course ends
+        * enrollment_end: Date enrollment ends
+        * enrollment_start: Date enrollment begins
+        * course_id: Course key
+        * name: Name of the course
+        * number: Catalog number of the course
+        * org: Name of the organization that owns the course
+        * short_description: A textual description of the course
+        * start: Date the course begins
+        * start_display: Readably formatted start of the course
+        * start_type: Hint describing how `start_display` is set. One of:
+            * `"string"`: manually set
+            * `"timestamp"`: generated form `start` timestamp
+            * `"empty"`: the start date should not be shown
 
-        Parameters:
-            username (optional):
-                The username of the specified user whose visible courses we
-                want to see.  Defaults to the current user.
+    **Parameters:**
 
-        Returns:
-            HttpResponse: 200 on success
+        username (optional):
+            The username of the specified user whose visible courses we
+            want to see.  Defaults to the current user.
 
-        Example Usage:
-            GET /api/courses/v1/[course_key_string]
-            200 OK
+    **Returns**
+
+        * 200 on success with above fields.
+        * 404 if the course is not available or cannot be seen.
 
         Example response:
+
             {
-                "blocks_url": "/api/courses/v1/blocks/?course_id=edX%2Ftoy%2F2012_Fall",
-                "course_image": "/c4x/edX/toy/asset/just_a_test.jpg",
-                "description": "A course about toys.",
+                "blocks_url": "/api/courses/v1/blocks/?course_id=edX%2Fexample%2F2012_Fall",
+                "media": {
+                    "course_image": {
+                        "uri": "/c4x/edX/example/asset/just_a_test.jpg",
+                        "name": "Course Image"
+                    }
+                },
+                "description": "An example course.",
                 "end": "2015-09-19T18:00:00Z",
                 "enrollment_end": "2015-07-15T00:00:00Z",
                 "enrollment_start": "2015-06-15T00:00:00Z",
-                "id": "edX/toy/2012_Fall",
-                "name": "Toy Course",
-                "number": "toy",
+                "id": "edX/example/2012_Fall",
+                "name": "Example Course",
+                "number": "example",
                 "org": "edX",
                 "start": "2015-07-17T12:00:00Z",
                 "start_display": "July 17, 2015",
                 "start_type": "timestamp"
             }
+    """
+
+    def get(self, request, course_key_string):
         """
+        GET /api/courses/v1/courses/{course_key_string}/
+        """
+
         username = request.query_params.get('username', request.user.username)
         try:
             course_key = CourseKey.from_string(course_key_string)
@@ -89,30 +106,61 @@ class CourseDetailView(APIView):
 
 class CourseListView(APIView):
     """
-    View class to list multiple courses
+    **Use Cases**
+
+        Request information on all courses visible to the specified user.
+
+    **Example Requests**
+
+        GET /api/courses/v1/courses/
+
+    **Response Values**
+
+        Body comprises a list of objects as returned by `CourseDetailView`.
+
+    **Parameters**
+
+        username (optional):
+            The username of the specified user whose visible courses we
+            want to see.  Defaults to the current user.
+
+    **Returns**
+
+        * 200 on success, with a list of course discovery objects as returned
+          by `CourseDetailView`.
+
+        * 404 if the specified user does not exist, or the requesting user does
+          not have permission to view their courses.
+
+        Example response:
+
+            [
+              {
+                "blocks_url": "/api/courses/v1/blocks/?course_id=edX%2Fexample%2F2012_Fall",
+                "media": {
+                  "course_image": {
+                    "uri": "/c4x/edX/example/asset/just_a_test.jpg",
+                    "name": "Course Image"
+                  }
+                },
+                "description": "An example course.",
+                "end": "2015-09-19T18:00:00Z",
+                "enrollment_end": "2015-07-15T00:00:00Z",
+                "enrollment_start": "2015-06-15T00:00:00Z",
+                "id": "edX/example/2012_Fall",
+                "name": "Example Course",
+                "number": "example",
+                "org": "edX",
+                "start": "2015-07-17T12:00:00Z",
+                "start_display": "July 17, 2015",
+                "start_type": "timestamp"
+              }
+            ]
     """
 
     def get(self, request):
         """
-        Request information on all courses visible to the specified user.
-
-        Body consists of a list of objects as returned by `CourseDetailView`.
-
-        Arguments:
-            request (HttpRequest)
-
-        Parameters:
-            username (optional):
-                The username of the specified user whose visible courses we
-                want to see.  Defaults to the current user.
-
-        Returns:
-            - A 404 response, if the specified user does not exist, or the
-              requesting user does not have permission to view their
-              courses.
-
-            - A 200 response, if the request is successful, with a list of
-              course discovery objects as returned by `CourseDetailView`.
+        GET /api/courses/v1/courses/{course_key_string}/
         """
         username = request.query_params.get('username', request.user.username)
 
